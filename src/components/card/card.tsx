46ef97f67e5cef222bad9981aa5703dc6754fe7e
drhyo <drhyomuhammad@gmail.com>
2023-07-10 14:16:19 +0700

    add integration

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -4,30 +4,46 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import { Link } from 'react-router-dom';
 import '../card/card.css'
+import axios from 'axios';
 
 const Card = () => {
+    const [content, setContent] = useState<any>([]);
+    const url = 'http://localhost:4000/pelatihan-card';
+        useEffect(() =>{
+        axios.get(url).then((response) => {
+            setContent(response.data.data);
+        })
+    }, [])
+
+    
     return (
         <div className='mx-auto max-w-[1910px] max-h-[1728px] relative'>
             <div className='sm:pt-20 sm:pb-20 sm-440:pt-20 sm-440:pb-20'>
                 <div className='container-card'>
-                    <div className="card-container">
-                        <div className="image-container">
-                            <div className="heading">Rp 375.000</div>
-                            <img src='/images/dokter.webp' alt="/" />
-                        </div>
-                        <div className="card-title">
-                            <h3 className='text'>Pelatihan 1</h3>
-                        </div>
-                        <div className="card-body">
-                            <p className='text'>
-                                <a href="/">Berisi penjelasan disini... </a>
-                                lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor</p>
-                        </div>
-                        <div className="btn">
-                            <button className="button"><Link to="/payment">Pesan Sekarang</Link></button>
-                        </div>
-                    </div>
-                    <div className="card-container">
+
+                    {
+                        content.map((res: any) => (
+                            <div className="card-container">
+                                <div className="image-container">
+                                    <div className="heading">{res.price}</div>
+                                    <img src='/images/dokter.webp' alt="/" />
+                                </div>
+                                <div className="card-title">
+                                    <h3 className='text'>{res.title}</h3>
+                                </div>
+                                <div className="card-body">
+                                    <p className='text'>{res.body.substring(0, 200)}</p>
+                                </div>
+                                <div className="btn">
+                                    <button className="button"><Link to="/payment">Pesan Sekarang</Link></button>
+                                </div>
+                            </div>
+
+                        ))
+                    }
+
+
+                    {/* <div className="card-container">
                         <div className="image-container">
                             <div className="heading">Rp 375.000</div>
                             <img src='/images/dokter.webp' alt="/" />
@@ -77,133 +93,134 @@ const Card = () => {
                         <div className="btn">
                             <button className="button"><Link to="/payment">Pesan Sekarang</Link></button>
                         </div>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </div>
     );
 };
 
-const Founding = () => {
-    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+// const Founding = () => {
+//     const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
-    useEffect(() => {
-        const handleResize = () => {
-            setWindowWidth(window.innerWidth);
-        };
+//     useEffect(() => {
+//         const handleResize = () => {
+//             setWindowWidth(window.innerWidth);
+//         };
 
-        window.addEventListener('resize', handleResize);
+//         window.addEventListener('resize', handleResize);
 
-        return () => {
-            window.removeEventListener('resize', handleResize);
-        };
-    }, []);
+//         return () => {
+//             window.removeEventListener('resize', handleResize);
+//         };
+//     }, []);
 
-    return windowWidth <= 440 ? <ImageSlider /> : <Card />;
-};
+//     return windowWidth <= 440 ? <ImageSlider /> : <Card />;
+// };
 
-const ImageSlider = () => {
-    const settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        responsive: [
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    dots: false,
-                },
-            },
-            {
-                breakpoint: 440,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    dots: true,
-                },
-            },
-        ],
-    };
+// const ImageSlider = () => {
+//     const settings = {
+//         dots: true,
+//         infinite: true,
+//         speed: 500,
+//         slidesToShow: 1,
+//         slidesToScroll: 1,
+//         autoplay: true,
+//         responsive: [
+//             {
+//                 breakpoint: 768,
+//                 settings: {
+//                     slidesToShow: 1,
+//                     slidesToScroll: 1,
+//                     dots: false,
+//                 },
+//             },
+//             {
+//                 breakpoint: 440,
+//                 settings: {
+//                     slidesToShow: 1,
+//                     slidesToScroll: 1,
+//                     dots: true,
+//                 },
+//             },
+//         ],
+//     };
 
-    return (
-        <div className="container-slider" style={{ padding: '20px' }}>
-            <Slider {...settings}>
-                <div className="card-container">
-                    <div className="image-container">
-                        <div className="heading">Rp 375.000</div>
-                        <img src='/images/dokter.webp' alt="/" />
-                    </div>
-                    <div className="card-title">
-                        <h3 className='text'>Pelatihan 1</h3>
-                    </div>
-                    <div className="card-body">
-                        <p className='text'>
-                            <a href="/">Berisi penjelasan disini... </a>
-                            lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor</p>
-                    </div>
-                    <div className="btn">
-                        <button className="button"><Link to="/payment">Pesan Sekarang</Link></button>
-                    </div>
-                </div>
-                <div className="card-container">
-                    <div className="image-container">
-                        <div className="heading">Rp 375.000</div>
-                        <img src='/images/dokter.webp' alt="/" />
-                    </div>
-                    <div className="card-title">
-                        <h3 className='text'>Pelatihan 2</h3>
-                    </div>
-                    <div className="card-body">
-                        <p className='text'>
-                            <a href="/">Berisi penjelasan disini... </a>
-                            lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor</p>
-                    </div>
-                    <div className="btn">
-                        <button className="button"><Link to="/payment">Pesan Sekarang</Link></button>
-                    </div>
-                </div>
-                <div className="card-container">
-                    <div className="image-container">
-                        <div className="heading">Rp 375.000</div>
-                        <img src='/images/dokter.webp' alt="/" />
-                    </div>
-                    <div className="card-title">
-                        <h3 className='text'>Pelatihan 3</h3>
-                    </div>
-                    <div className="card-body">
-                        <p className='text'>
-                            <a href="/">Berisi penjelasan disini... </a>
-                            lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor</p>
-                    </div>
-                    <div className="btn">
-                        <button className="button"><Link to="/payment">Pesan Sekarang</Link></button>
-                    </div>
-                </div>
-                <div className="card-container">
-                    <div className="image-container">
-                        <div className="heading">Rp 375.000</div>
-                        <img src='/images/dokter.webp' alt="/" />
-                    </div>
-                    <div className="card-title">
-                        <h3 className='text'>Pelatihan 4</h3>
-                    </div>
-                    <div className="card-body">
-                        <p className='text'>
-                            <a href="/">Berisi penjelasan disini... </a>
-                            lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor</p>
-                    </div>
-                    <div className="btn">
-                        <button className="button"><Link to="/payment">Pesan Sekarang</Link></button>
-                    </div>
-                </div>
-            </Slider>
-        </div>
-    );
-};
+//     return (
+//         <div className="container-slider" style={{ padding: '20px' }}>
+//             <Slider {...settings}>
+//                 <div className="card-container">
+//                     <div className="image-container">
+//                         <div className="heading">Rp 375.000</div>
+//                         <img src='/images/dokter.webp' alt="/" />
+//                     </div>
+//                     <div className="card-title">
+//                         <h3 className='text'>Pelatihan 1</h3>
+//                     </div>
+//                     <div className="card-body">
+//                         <p className='text'>
+//                             <a href="/">Berisi penjelasan disini... </a>
+//                             lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor</p>
+//                     </div>
+//                     <div className="btn">
+//                         <button className="button"><Link to="/payment">Pesan Sekarang</Link></button>
+//                     </div>
+//                 </div>
+//                 <div className="card-container">
+//                     <div className="image-container">
+//                         <div className="heading">Rp 375.000</div>
+//                         <img src='/images/dokter.webp' alt="/" />
+//                     </div>
+//                     <div className="card-title">
+//                         <h3 className='text'>Pelatihan 2</h3>
+//                     </div>
+//                     <div className="card-body">
+//                         <p className='text'>
+//                             <a href="/">Berisi penjelasan disini... </a>
+//                             lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor</p>
+//                     </div>
+//                     <div className="btn">
+//                         <button className="button"><Link to="/payment">Pesan Sekarang</Link></button>
+//                     </div>
+//                 </div>
+//                 <div className="card-container">
+//                     <div className="image-container">
+//                         <div className="heading">Rp 375.000</div>
+//                         <img src='/images/dokter.webp' alt="/" />
+//                     </div>
+//                     <div className="card-title">
+//                         <h3 className='text'>Pelatihan 3</h3>
+//                     </div>
+//                     <div className="card-body">
+//                         <p className='text'>
+//                             <a href="/">Berisi penjelasan disini... </a>
+//                             lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor</p>
+//                     </div>
+//                     <div className="btn">
+//                         <button className="button"><Link to="/payment">Pesan Sekarang</Link></button>
+//                     </div>
+//                 </div>
+//                 <div className="card-container">
+//                     <div className="image-container">
+//                         <div className="heading">Rp 375.000</div>
+//                         <img src='/images/dokter.webp' alt="/" />
+//                     </div>
+//                     <div className="card-title">
+//                         <h3 className='text'>Pelatihan 4</h3>
+//                     </div>
+//                     <div className="card-body">
+//                         <p className='text'>
+//                             <a href="/">Berisi penjelasan disini... </a>
+//                             lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor</p>
+//                     </div>
+//                     <div className="btn">
+//                         <button className="button"><Link to="/payment">Pesan Sekarang</Link></button>
+//                     </div>
+//                 </div>
+//             </Slider>
+//         </div>
+//     );
+// };
 
-export default Founding;
+export default Card;
+//  Founding;
--- src/components/sosmed/sosmed.tsx
+++ src/components/sosmed/sosmed.tsx
@@ -3,24 +3,26 @@ import Carousel from 'react-multi-carousel';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import axios from 'axios';
 
 interface SocialMediaProfileProps {
     name: string;
     subtitle: string;
     bio: string;
+    image: string;
     link1: string;
     link2: string;
 }
 
-const SocialMediaProfile: React.FC<SocialMediaProfileProps> = ({ name, subtitle, bio, link1, link2 }) => {
+const SocialMediaProfile: React.FC<SocialMediaProfileProps> = ({ name, subtitle, bio, link1, link2, image }) => {
     return (
         <>
             <div className='mx-auto max-w-[1910px] max-h-[1800px] relative'>
                 <div className='bg-white my-12 pb-6 m-10 w-full justify-center items-center overflow-hidden md:max-w-sm rounded-lg mx-auto drop-shadow-xl'>
                     <div className='relative h-[150px]'></div>
                     <div className='relative mx-auto h-24 w-24 -my-20 bottom-10'>
-                        <div className='object-cover w-full h-full'>
-                            <img src='/images/avatar.webp' alt='' />
+                        <div className='object-cover w-full h-full '>
+                            <img src={image} alt=''/>
                         </div>
                     </div>
                     <div className='mt-20 p-5'>
@@ -30,10 +32,10 @@ const SocialMediaProfile: React.FC<SocialMediaProfileProps> = ({ name, subtitle,
                     </div>
                     <div className='mt-6 pt-3 flex flex-wrap mx-6 border-t'>
                         <div className='text-xs mr-2 my-1 uppercase tracking-wider border px-2 text-indigo-600 border-indigo-600 hover:bg-indigo-600 hover:text-indigo-100 cursor-default'>
-                            {link1}
+                            <a href={link1}>Sosial Media</a>
                         </div>
                         <div className='text-xs mr-2 my-1 uppercase tracking-wider border px-2 text-indigo-600 border-indigo-600 hover:bg-indigo-600 hover:text-indigo-100 cursor-default'>
-                            {link2}
+                            <a href={link2}> Sosial Media </a>
                         </div>
                     </div>
                 </div>
@@ -43,12 +45,34 @@ const SocialMediaProfile: React.FC<SocialMediaProfileProps> = ({ name, subtitle,
 };
 
 const Desktop = () => {
+    const [content, setContent] = useState<any>([]);
+    const url = 'http://localhost:4000/personal-card';
+        useEffect(() =>{
+        axios.get(url).then((response) => {
+            setContent(response.data.data);
+        })
+    }, [])
+
     return (
         <div className='flex'>
-            <SocialMediaProfile
+            {
+                content.map((res: any) => (
+                    <SocialMediaProfile 
+                        name={res.name}
+                        subtitle={res.title}
+                        bio='Some quick example text to build on the card title and make up the bulk of the card content.'
+                        image={res.image_personal}
+                        link1={res.medsos_1}
+                        link2={res.medsos_2}
+                    />
+                    
+                ))
+            }
+            {/* <SocialMediaProfile
                 name='Nama'
                 subtitle='Card Subtitle'
                 bio='Some quick example text to build on the card title and make up the bulk of the card content.'
+                image='/images/avatar.webp'
                 link1='social media 1'
                 link2='Social media 2'
             />
@@ -56,93 +80,91 @@ const Desktop = () => {
                 name='Nama'
                 subtitle='Card Subtitle'
                 bio='Some quick example text to build on the card title and make up the bulk of the card content.'
+                image='/images/avatar.webp'
                 link1='social media 1'
                 link2='Social media 2'
-            />
-            <SocialMediaProfile
-                name='Nama'
-                subtitle='Card Subtitle'
-                bio='Some quick example text to build on the card title and make up the bulk of the card content.'
-                link1='social media 1'
-                link2='Social media 2'
-            />
+            /> */}
         </div>
     );
 };
 
-const Founding = () => {
-    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+// const Founding = () => {
+//     const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
-    useEffect(() => {
-        const handleResize = () => {
-            setWindowWidth(window.innerWidth);
-        };
+//     useEffect(() => {
+//         const handleResize = () => {
+//             setWindowWidth(window.innerWidth);
+//         };
 
-        window.addEventListener('resize', handleResize);
+//         window.addEventListener('resize', handleResize);
 
-        return () => {
-            window.removeEventListener('resize', handleResize);
-        };
-    }, []);
+//         return () => {
+//             window.removeEventListener('resize', handleResize);
+//         };
+//     }, []);
 
-    return windowWidth <= 440 ? <Mobile /> : <Desktop />;
-};
+//     return windowWidth <= 440 ? <Mobile /> : <Desktop />;
+// };
 
-const Mobile = () => {
-    const settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        responsive: [
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    dots: false,
-                },
-            },
-            {
-                breakpoint: 440,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    dots: true,
-                },
-            },
-        ],
-    };
+// const Mobile = () => {
+//     const settings = {
+//         dots: true,
+//         infinite: true,
+//         speed: 500,
+//         slidesToShow: 1,
+//         slidesToScroll: 1,
+//         autoplay: true,
+//         responsive: [
+//             {
+//                 breakpoint: 768,
+//                 settings: {
+//                     slidesToShow: 1,
+//                     slidesToScroll: 1,
+//                     dots: false,
+//                 },
+//             },
+//             {
+//                 breakpoint: 440,
+//                 settings: {
+//                     slidesToShow: 1,
+//                     slidesToScroll: 1,
+//                     dots: true,
+//                 },
+//             },
+//         ],
+//     };
 
-    return (
-        <div style={{ position: 'relative', zIndex: '0', padding: '30px' }}>
-            <Slider {...settings}>
-                <SocialMediaProfile
-                    name='Nama'
-                    subtitle='Card Subtitle'
-                    bio='Some quick example text to build on the card title and make up the bulk of the card content.'
-                    link1='social media 1'
-                    link2='Social media 2'
-                />
-                <SocialMediaProfile
-                    name='Nama'
-                    subtitle='Card Subtitle'
-                    bio='Some quick example text to build on the card title and make up the bulk of the card content.'
-                    link1='social media 1'
-                    link2='Social media 2'
-                />
-                <SocialMediaProfile
-                    name='Nama'
-                    subtitle='Card Subtitle'
-                    bio='Some quick example text to build on the card title and make up the bulk of the card content.'
-                    link1='social media 1'
-                    link2='Social media 2'
-                />
-            </Slider>
-        </div>
-    );
-};
+//     return (
+//         <div style={{ position: 'relative', zIndex: '0', padding: '30px' }}>
+//             <Slider {...settings}>
+//                 <SocialMediaProfile
+//                     name='Nama'
+//                     subtitle='Card Subtitle'
+//                     bio='Some quick example text to build on the card title and make up the bulk of the card content.'
+//                     image='/images/avatar.webp'
+//                     link1='social media 1'
+//                     link2='Social media 2'
+//                 />
+//                 <SocialMediaProfile
+//                     name='Nama'
+//                     subtitle='Card Subtitle'
+//                     bio='Some quick example text to build on the card title and make up the bulk of the card content.'
+//                     image='/images/avatar.webp'
+//                     link1='social media 1'
+//                     link2='Social media 2'
+//                 />
+//                 <SocialMediaProfile
+//                     name='Nama'
+//                     subtitle='Card Subtitle'
+//                     bio='Some quick example text to build on the card title and make up the bulk of the card content.'
+//                     image='/images/avatar.webp'
+//                     link1='social media 1'
+//                     link2='Social media 2'
+//                 />
+//             </Slider>
+//         </div>
+//     );
+// };
 
-export default Founding;
+export default Desktop;
+//  Founding;

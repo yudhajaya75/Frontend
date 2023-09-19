cee62dd7bca7618ff883b814d01cbe8ac640d11d
Deni setiawan <denistwn67@gmail.com>
2023-09-19 23:22:13 +0700

    feat:riwayat transaksi

--- src/components/content/content2.tsx
+++ src/components/content/content2.tsx
@@ -1,43 +1,81 @@
-import React, { useState } from 'react';
-import useTransaction from '../../hooks/useTransaction';
+import React, { useEffect, useState } from "react";
+import Button from "@mui/material/Button";
+import { HTTPAruna } from "../../services/handlerApi";
+import { Transaction, style } from "./profile/style";
+import ModalSection from "./profile/modal";
 
 const Profile: React.FC = () => {
-    const [activePage, setActivePage] = useState('');
-    const { email, setEmail, isFetchingData, isLoggedIn, } = useTransaction();
+  const [activePage, setActivePage] = useState("");
+  const [data, setData] = useState<Transaction[]>([]);
+  const [open, setOpen] = React.useState(false);
+  const [transactionId, setTransactionId] = useState<string | null>(null);
 
+  const handleNavbarClick = (page: string) => {
+    setActivePage(page);
+  };
 
-    const handleNavbarClick = (page: string) => {
-        setActivePage(page);
-    };
+  const getTransaction = async () => {
+    try {
+      const res = await HTTPAruna.get(`/api/transactions?populate=*`);
+      setData(res.data.data);
+    } catch (error) {}
+  };
+  useEffect(() => {
+    getTransaction();
+  }, []);
 
-    return (
+  const filterData = data.filter(
+    (item) =>
+      item.attributes.users.data.attributes.email ===
+      localStorage.getItem("email")
+  );
+
+  const handleOpenModal = (transactionId: string) => {
+    setTransactionId(transactionId);
+    console.log("transactionId", transactionId);
+    setOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setTransactionId(null);
+    setOpen(false);
+  };
+
+  console.log("filter", filterData);
+  console.log("transactionId", transactionId);
+
+  return (
+    <>
+      {filterData.map((item, index) => (
         <>
-            <div>
-                <div className="bg-white w-[80rem] mx-auto mt-5 rounded-lg shadow-lg py-5">
-                    <ul className="flex">
-                        <li
-                            className={`ml-5 text-[24px] p-5 ${activePage === 'riwayat' ? 'text-[#002157] bg-[#f0f2f5]' : 'text-[#002157]'
-                                } rounded-md cursor-pointer duration-500`}
-                            onClick={() => handleNavbarClick('riwayat')}
-                        >
-                            <a href="#riwayat">Riwayat Pembelian</a>
-                        </li>
-                    </ul>
-                </div>
-                <div className='flex items-center justify-center p-10'>
-                    <div className='h-[560px] bg-white w-[80rem] mx-auto rounded-lg shadow-lg'>
-                        <div className='mt-[250px] text-center'>
-                            <div id="riwayat">
-                                {activePage === 'riwayat' &&
-                                    <p>{isLoggedIn}</p>
-                                }
-                            </div>
-                        </div>
-                    </div>
-                </div>
+          <div>
+            <div className="bg-white w-[80rem] mx-auto mt-5 rounded-lg shadow-lg py-5">
+              <ul className="flex">
+                <li
+                  className={`ml-5 text-[24px] p-5 ${
+                    activePage === "riwayat"
+                      ? "text-[#002157] bg-[#f0f2f5]"
+                      : "text-[#002157]"
+                  } rounded-md cursor-pointer duration-500`}
+                  onClick={() => handleNavbarClick("riwayat")}
+                >
+                  <Button onClick={() => handleOpenModal(item.id.toFixed())}>
+                    Riwayat Pembelian{" "}
+                    {item.attributes.users.data.attributes.email}
+                  </Button>
+                </li>
+              </ul>
             </div>
+          </div>
         </>
-    );
+      ))}
+      <ModalSection
+        open={open}
+        onClose={handleCloseModal}
+        transactionId={transactionId}
+      />
+    </>
+  );
 };
 
 export default Profile;
--- src/pages/profile/profile.tsx
+++ src/pages/profile/profile.tsx
@@ -1,19 +1,20 @@
-import Navbar from '../../components/navbar/navbar'
-import Content from '../../components/content/content2'
-import Content2 from '../../components/content/content3'
-import Footer from '../../components/footer/footer'
+import Navbar from "../../components/navbar/navbar";
+import Content from "../../components/content/content2";
+import Content2 from "../../components/content/content3";
+import Footer from "../../components/footer/footer";
+import GlobalLayout from "../../layouts/GlobalLayout";
 
 const Profile = (props: { email: string }) => {
-    return (
-        <>
-            <div className='mx-auto max-w-[1910px] relative'>
-                <Navbar accountEmail={props.email} />
-                <Content2 accountEmail={props.email} />
-                <Content />
-                <Footer />
-            </div>
-        </>
-    )
-}
+  const userEmail = localStorage.getItem("email");
+  const user = localStorage.getItem("user");
+  return (
+    <>
+      <GlobalLayout accountEmail={user || ""}>
+        <Content2 accountEmail={userEmail || ""} />
+        <Content />
+      </GlobalLayout>
+    </>
+  );
+};
 
-export default Profile
+export default Profile;

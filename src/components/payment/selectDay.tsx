5c95a164acdaece3f9e0d2fa31a009331a8ab613
Mahrus Rohisyam <73371041+mahrus-rohisyam@users.noreply.github.com>
2023-09-29 22:03:35 +0700

    fix:PUT field (#22)
    Co-authored-by: Deni setiawan <denistwn67@gmail.com>

--- src/components/payment/selectDay.tsx
+++ src/components/payment/selectDay.tsx
@@ -1,16 +1,19 @@
-
 const SelectDay = () => (
-    <select
-        name="day"
-        id="tanggal"
-        className='p-2 2xl:w-[130px] sm-440:text-[10px] 2xl:text-[14px] sm-440:w-[160px] outline-none no-underline rounded-md border border-[#8D8D8D]'
-        data-te-select-placeholder="Example placeholder"
-    >
-        <option value="" disabled selected>Tanggal</option>
-        {[...Array(31)].map((_, index) => (
-            <option key={index} value={index + 1}>{index + 1}</option>
-        ))}
-    </select>
+  <select
+    name="day"
+    id="tanggal"
+    className="p-2 2xl:w-[130px] sm-440:text-[10px] 2xl:text-[14px] sm-440:w-[160px] outline-none no-underline rounded-md border border-[#8D8D8D]"
+    data-te-select-placeholder="Example placeholder"
+  >
+    <option value="" disabled selected>
+      Tanggal
+    </option>
+    {[...Array(31)].map((_, index) => (
+      <option key={index} value={index + 1}>
+        {index + 1}
+      </option>
+    ))}
+  </select>
 );
 
 export default SelectDay;
